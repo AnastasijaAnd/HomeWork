397cb471f5c19f5afc7bf0386ff9e0bd102485d5
AnastasijaAnd <rosaamina@mail.ru>
2023-04-13 01:02:43 +0300

    second

---
+++ HW1 ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Программа, принимает на вход три числа и выдает максимальное из этих чисел.
+
+System.Console.WriteLine("число 1: ");
+int a = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("число 2: ");
+int b = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("число 3: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (max<b){
+    max = b;
+}
+if (max<c){
+    max =c;
+}
+Console.WriteLine($"максимальное число {max}.");
