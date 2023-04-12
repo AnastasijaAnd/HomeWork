3b6a61a37daea03debe5e74dc326f41a4072250b
AnastasijaAnd <rosaamina@mail.ru>
2023-04-13 01:07:37 +0300

    third

---
+++ HW1 ex3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Программа, на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+
+System.Console.WriteLine("число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0){
+    Console.WriteLine($"число {a} - четное.");
+}
+else{
+    Console.WriteLine($"число {a} - не четное.");
+}
