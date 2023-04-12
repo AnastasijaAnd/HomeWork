3b94f73da7dc5d9748b844250cb7d9a8f4670187
AnastasijaAnd <rosaamina@mail.ru>
2023-04-13 00:46:42 +0300

    first

---
+++ HW1 ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+
+System.Console.WriteLine("число 1: ");
+int a = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("число 2: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a>b){
+    Console.WriteLine($"большее число {a} , меньшее число {b}.");
+}
+else{
+    Console.WriteLine($"большее число {b} , меньшее число {a}.");
+}
+
