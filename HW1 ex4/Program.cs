f4a8faf6956258156d2dda35b95eafa4e5f4e367
AnastasijaAnd <rosaamina@mail.ru>
2023-04-13 01:11:55 +0300

    fourth

---
+++ HW1 ex4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Программа, на входе принимает число (N), а на выходе показывает все четные числа от 1 до N.
+
+System.Console.WriteLine("введите число N: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write($"от 1 до  {a} - четные числа:");
+
+for (int i = 1; i <=a; i++){
+    if (i % 2 == 0){
+        Console.Write(i + "  ");
+    }
+}
