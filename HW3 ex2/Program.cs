6f48afd6d36cbe39b7f742184d83961de024776a
AnastasijaAnd <rosaamina@mail.ru>
2023-04-19 23:30:51 +0300

    322

--- HW3 ex2/Program.cs
+++ HW3 ex2/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите координаты первой точки пространства в формате х,у,z: ");
+﻿/* ДЗ3 ЗАДАЧА 2  Программа, принимает на вход координаты двух точек и 
+находит расстояние между ними в 3D пространстве.*/
+
+Console.WriteLine("Введите координаты первой точки пространства в формате х,у,z: ");
 string input1 = Console.ReadLine();
 string[] splitInput1 = input1.Split(',');
 int x1 = int.Parse(splitInput1[0]);
@@ -11,8 +14,6 @@ string[] splitInput2 = input2.Split(',');
 int x2 = int.Parse(splitInput2[0]);
 int y2 = int.Parse(splitInput2[1]);
 int z2 = int.Parse(splitInput2[2]);
-
-
 
 Console.WriteLine($"Расстояние между точками равно: {Distance(x1, y1, z1, x2, y2, z2)}.");
 
